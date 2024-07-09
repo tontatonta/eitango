31a78bcc1bcf7c16c55d7b4768b3e60e9ad779e3
Inomata <tonyhawkingsexample@example.com>
2024-07-09 09:54:56 +0900

    add Sidebar

--- eitango/frontend/src/components/Addwords.jsx
+++ eitango/frontend/src/components/Addwords.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Appwords = () => {
+const Words = () => {
     return (
         <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800">
         </header>
     );
 };
 
-export default Appwords;
+export default Words;
