fc5a8e944bad625f2e570f5172f98dbc6fee4980
Inomata <tonyhawkingsexample@example.com>
2024-07-16 17:59:12 +0900

    modify sidebar and headerfile

--- eitango/frontend/src/components/Header.jsx
+++ eitango/frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800">
             <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
                 <div class="flex items-center justify-between">
-                    <a class="flex-none text-xl font-semibold dark:text-white" href="#">Eitangooo!!</a>
+                    <Link class="flex-none text-xl font-semibold dark:text-white" to="/">Eitangooo!!</Link>
                     <div class="sm:hidden">
                         <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
                             <svg class="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
